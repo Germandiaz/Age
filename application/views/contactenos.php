1aa89e8bf8098bd5457a5a310e3905cd917a09f4
Jesse cogollo <cogollo87@gmail.com>
2012-08-23 23:39:02 -0500

    Prueba de actualización
    Prueba de actualización

--- application/views/contactenos.php
+++ application/views/contactenos.php
@@ -1,7 +1,7 @@
 <div>
     <h2>Cont&#225;ctenos</h2>
     <div align="justify">
-        <p>AGE</p>
+        <p>AGE (Administración Gerontologica)</p>
         <p>Ubicaci&#243;n: Calle1 # 2-3 Barrio casa</p>
         <p>Telefono: 2345678</p>
         <p>Contacto: Jesse Cogollo</p>
