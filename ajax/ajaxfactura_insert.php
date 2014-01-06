fac99068311b87f826cbe44a421c98698151d89c
Germandiaz <cherman619@hotmail.com>
2014-01-05 19:05:50 -0500

    Update ajaxfactura_insert.php
    - Cambio del include_once para que la ruta no quede absoluta. Se debe tener encuenta que si no funciona se debe cambiar por el require_once ya que este esto depende de la configuración del host
    - Modificación en estructura condicional para validar que el envió de los conceptos no sea null
    - Adición de class error y éxito en los los divs que presentan los mensajes esperados

--- ajax/ajaxfactura_insert.php
+++ ajax/ajaxfactura_insert.php
@@ -1,23 +1,44 @@
 <?php 
-//include_once 'factura_model.php';
-require_once ("../application/models/factura_model.php");
+
+// require_once ("../application/models/factura_model.php"); // include para el Host
+// modificación de ruta include para que no sea absoluta
+include_once '..\application\models\factura_model.php';
 if (isset ($_SERVER['HTTP_X_REQUESTED_WITH']) && $_SERVER['HTTP_X_REQUESTED_WITH'] == 'XMLHttpRequest'){
 
-    $factura = new factura_model;    
-    $factura->insertarfactura($_POST);
-  //   if(isset ($dat)){      
+  //   if(isset ($dat)){  
+    //echo $_POST['concepto4'];   
+  //   echo $_POST['concepto3'];  
+  //   echo $_POST['concepto2'];  
+   //  echo $_POST['concepto1'];  
   //  echo $_POST['idusuario'];
- //   echo $_POST['numerocampos'];
- //   echo $_POST['idusuarioelabora'];
-  //  echo $_POST['concepto1'];
+  //  echo $_POST['numerocampos'];
+
+    foreach ($_POST as $key => $value)
+    {
+      if (empty($value)){
+           
+         $estadoregistro = "<div class='mensajeerror'><span>Error</span>, debe ingresar datos en los conceptos de la factura</div>";
+         break;              
+      }else{
+          $factura = new factura_model;  
+          $estadoregistro = $factura->insertarfactura($_POST);
+          $estadoregistro = "<div class='mensajeexito'>Factura registrada correctamente</div>";
+          break; 
+      }
+    }
+    
     ?>
-<div>
-    <p><h2>Formulario enviado correctamente</h2></p>
-</div>
+        <div>
+   
+                <p><h2><?php echo $estadoregistro?></h2></p>
+    
+        </div>
 <?php 
 }else{
     ?>
+<div>
     <p><h2>Se requiere activar Javascript</h2></p> 
+</div>
 <?php }
  ?>
 
