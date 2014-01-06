9858e04f47602291d54eac10942065905b9b1630
Germandiaz <cherman619@hotmail.com>
2014-01-05 22:23:18 -0500

    Update encabezadoingresado.php
    Adición de hoja de estilo para calendarios y autocompletar

--- application/views/includes/encabezadoingresado.php
+++ application/views/includes/encabezadoingresado.php
@@ -6,6 +6,8 @@
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
     	<meta name="description" content="Administración Gerontologica">
     	<meta name="author" content="@jessecogollo">
+    	<!-- Hoja de estilos para el autocompletar y calendarios, esto aplica para todo elemento que se utilice en el DOM-->
+    	<link rel="stylesheet" href="<?php echo base_url(); ?>css/jquery-ui-1.8.17.custom.css" type="text/css" media="screen" />
     	<!-- Librerias necesarias de cargar en el header para la implementación del autocompletar -->
     	<script type="text/javascript" src="<?php echo base_url(); ?>js/jquery-1.7.1.min.js"></script>
         <script type="text/javascript" src="<?php echo base_url(); ?>js/jquery-ui-1.8.17.custom.min.js"></script> 
