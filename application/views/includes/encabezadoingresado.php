da437c5d992ce436fccfc656d6c420fa4e89eb59
Germandiaz <cherman619@hotmail.com>
2014-01-05 22:15:10 -0500

    Update encabezadoingresado.php
    Adición de librerías jquery

--- application/views/includes/encabezadoingresado.php
+++ application/views/includes/encabezadoingresado.php
@@ -6,5 +6,8 @@
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
     	<meta name="description" content="Administración Gerontologica">
     	<meta name="author" content="@jessecogollo">
+    	<!-- Librerias necesarias de cargar en el header para la implementación del autocompletar -->
+    	<script type="text/javascript" src="<?php echo base_url(); ?>js/jquery-1.7.1.min.js"></script>
+        <script type="text/javascript" src="<?php echo base_url(); ?>js/jquery-ui-1.8.17.custom.min.js"></script> 
     </head>
     <body>
